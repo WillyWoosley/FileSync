a41e5d7147acf108770e4b1e5d6915f37a7a8aa2
WillyWoosley <wdwoosley@gmail.com>
2017-08-10 11:55:25 -0700

    intro to computing started

--- C/detab.c
+++ C/detab.c
@@ -4,12 +4,19 @@
 
 int main() {
 
+    char c;
+    int len = 0;
+
     while ((c = getchar()) != EOF) {
         if (c == '\t')
-            for (int i=0; i<TABLEN; i++)
+            for (int i=0; i < TABLEN; i++) {
                 putchar(' ');
-        else
+                len++;
+            }
+        else {
             putchar(c);
+            len++;
+        }
     }
 
     return 0;
