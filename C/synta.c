c224895c265d79ab952114a1299335b38609cb55
WillyWoosley <wdwoosley@gmail.com>
2017-08-20 11:07:26 -0700

    math, scripting notes

---
+++ C/synta.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define MAXCHAR 1000
+
+int main() {
+
+
+    int inbloc=0, incom=0, instr=0;
+    int curly=0, square=0, paren=0;
+
+    /*fuck you buffers dont make it easier
+     * it just requires going through a single letter at a time and
+     * seeing if every single fucking letter denotes somethingf special
+     * and setting bools based upon that
+     * beyond that you just count shit and make sure that they match
+     */
+
+
+}
