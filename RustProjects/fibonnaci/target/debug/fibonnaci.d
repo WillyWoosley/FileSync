8a387bc5403605dfeb34080186ce44d3023c166c
wdwoo <wdwoosley@gmail.com>
2017-09-29 13:34:28 -0700

    merge commit

--- RustProjects/fibonnaci/target/debug/fibonnaci.d
+++ RustProjects/fibonnaci/target/debug/fibonnaci.d
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 /home/maw/SyncSpace/RustProjects/fibonnaci/target/debug/fibonnaci: /home/maw/SyncSpace/RustProjects/fibonnaci/src/main.rs
+=======
+/home/zugzwang/SyncSpace/RustProjects/fibonnaci/target/debug/fibonnaci: /home/zugzwang/SyncSpace/RustProjects/fibonnaci/src/main.rs
+>>>>>>> merge commit
